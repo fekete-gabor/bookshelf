67d6445c53b69bb0e534006e947246c0d1e003b7
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-01-06 16:06:26 +0100

    created alert messages

---
+++ src/utils/alertMessages.js
@@ -0,0 +1,5 @@
+const add_success = "Added to favourites!";
+const remove_success = "Removed from favourites!";
+const error = "Something went wrong!";
+
+export { add_success, remove_success, error };
