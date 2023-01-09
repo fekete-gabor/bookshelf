4fa0af447fb1c09ec931f082f6054166f135a8b2
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-01-09 14:56:46 +0100

    created error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,76 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const ErrorPage = () => {
   return (
     <Wrapper>
-      <h2>ErrorPage</h2>
+      <div className="text-container">
+        <h1>
+          <span>404 </span>page not found
+        </h1>
+        <h2>the page you are looking for does not exist</h2>
+        <h3>no worries though, you got plenty of options left</h3>
+      </div>
+      <div className="btn-container">
+        <Link to="/search">
+          <button className="btn">search for books</button>
+        </Link>
+        <Link to="/my-bookshelf">
+          <button className="btn">check my favourite books</button>
+        </Link>
+        <Link to="/">
+          <button className="btn">back to home page</button>
+        </Link>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  text-align: center;
+
+  .btn-container,
+  .text-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #221440;
+    span {
+      color: #f29441;
+    }
+    h1 {
+      color: #d097f5;
+    }
+    h2 {
+      padding: 0.5rem;
+    }
+    h2,
+    h3 {
+      color: var(--primary-white);
+    }
+  }
+
+  .btn-container {
+    background: var(--primary-white);
+  }
+
+  .btn {
+    background: #d097f5;
+    margin: 0.5rem 0;
+    font-size: 2rem;
+    &:hover {
+      background: #221440;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
 
 export default ErrorPage;
