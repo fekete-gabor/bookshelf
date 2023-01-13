a013e055fe99d3d59a8c93f7fb26b214de1e6704
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-01-13 16:10:41 +0100

    temporarily removed everything from home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,15 @@
-import { useAppContext } from "../context/app_context";
 import styled from "styled-components";
 
 const Home = () => {
-  const { favouriteBook } = useAppContext();
-
   return (
     <Wrapper>
-      {favouriteBook?.singleBook ? (
-        <div className="book">
-          <h2>{`id: ${favouriteBook.singleBook.id}`}</h2>
-          <h2>{`title: ${favouriteBook.singleBook.title}`}</h2>
-          <div className="">
-            <h2>authors:</h2>
-            {favouriteBook.singleBook.authors.map((author, i) => {
-              return (
-                <h2 key={i} className="author">
-                  {author}
-                </h2>
-              );
-            })}
-          </div>
-        </div>
-      ) : (
-        <h2>looks like there is nothing here, try to add some books</h2>
-      )}
+      return <h2>home</h2>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
   height: 100vh;
-  .book {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    gap: 1rem;
-  }
-
-  .author {
-    color: dodgerblue;
-  }
 `;
 
 export default Home;
