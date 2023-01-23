bbc3483c08e9a02a56497441a1f64fce5492eed3
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-01-23 16:38:59 +0100

    capitalized alert messages

--- src/utils/alertMessages.js
+++ src/utils/alertMessages.js
@@ -1,6 +1,12 @@
 import { toast } from "react-toastify";
 
 export const alertMessages = (type, msg) => {
+  msg = msg
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
   if (type === "success") return toast.success(msg);
   if (type === "warning") return toast.warning(msg);
   if (type === "error") return toast.error(msg);
