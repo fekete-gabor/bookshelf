4b01aafa4e87409e6479772334a9344c6ba513a7
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-01-24 13:50:30 +0100

    removed logout functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,9 @@
-import axios from "axios";
-import { alertMessages } from "../utils/alertMessages";
-import { useAppContext } from "../context/app_context";
 import styled from "styled-components";
 
 const Home = () => {
-  const { removeUser } = useAppContext();
-
-  const logout = async () => {
-    try {
-      const response = await axios.delete("/api/v1/auth/logout");
-      await removeUser();
-      alertMessages("success", `${response.data}`);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
-
   return (
     <Wrapper>
       <h2>home</h2>
-      <button onClick={() => logout()}>log out</button>
     </Wrapper>
   );
 };
