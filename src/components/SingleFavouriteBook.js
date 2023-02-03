99d8a625e9ff8dba845762568abcbc30dba9c2d3
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-03 13:56:04 +0100

    moved descriptions and buttons into separate components

--- src/components/SingleFavouriteBook.js
+++ src/components/SingleFavouriteBook.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
-import { Link, Navigate } from "react-router-dom";
-import { CustomDescription } from "./index";
+import { Navigate } from "react-router-dom";
+import {
+  SingleBookDescriptions,
+  SingleFavouriteBookButtons,
+  SingleFavouriteBookEdit,
+} from "./index";
 import notFound from "../assets/404.png";
 import { useParams } from "react-router-dom";
 import { useAppContext } from "../context/app_context";
-import { alertMessages } from "../utils/alertMessages";
 import styled from "styled-components";
 
 const SingleFavouriteBook = () => {
@@ -13,41 +16,16 @@ const SingleFavouriteBook = () => {
     isError,
     fetchSingleBookFromMongoDB,
     singleFavouriteBook,
-    removeFromFavourite,
   } = useAppContext();
 
   const { id } = useParams();
-
-  const removeBook = async () => {
-    try {
-      alertMessages("error", "Removed from favourites!");
-      await removeFromFavourite(id);
-    } catch (error) {
-      console.log(error);
-      alertMessages("warning", "Something went wrong!");
-    }
-  };
 
   useEffect(() => {
     fetchSingleBookFromMongoDB(id);
     // eslint-disable-next-line
   }, [id]);
 
-  const {
-    title,
-    subtitle,
-    authors,
-    averageRating,
-    myRating,
-    ratingsCount,
-    categories,
-    description,
-    language,
-    pageCount,
-    publishedDate,
-    publisher,
-    image,
-  } = singleFavouriteBook;
+  const { title, image } = singleFavouriteBook;
 
   if (isLoading) {
     return (
@@ -67,91 +45,10 @@ const SingleFavouriteBook = () => {
         <header>
           <img src={image || notFound} alt={title} />
         </header>
-        <article>
-          <CustomDescription
-            fieldName="title"
-            className="title"
-            showFieldName
-            value={title}
-          />
-          <CustomDescription
-            fieldName="subtitle"
-            className="subtitle"
-            showFieldName
-            value={subtitle}
-          />
-          <CustomDescription
-            fieldName="authors"
-            className="authors"
-            showFieldName
-            value={authors}
-          />
-          <CustomDescription
-            fieldName="average rating"
-            className="averageRating"
-            showFieldName
-            value={averageRating && `${averageRating} / 5`}
-          />
-          <CustomDescription
-            fieldName="my rating"
-            className="myRating"
-            showFieldName
-            value={`${myRating} / 5`}
-          />
-          <CustomDescription
-            fieldName="ratings count"
-            className="ratingsCount"
-            showFieldName
-            value={ratingsCount}
-          />
-          <CustomDescription
-            fieldName="categories"
-            className="categories"
-            showFieldName
-            value={categories}
-          />
-          <CustomDescription
-            fieldName="description"
-            className="description"
-            value={description}
-          />
-          <CustomDescription
-            fieldName="language"
-            className="language"
-            showFieldName
-            value={language}
-          />
-          <CustomDescription
-            fieldName="page count"
-            className="pageCount"
-            showFieldName
-            value={pageCount}
-          />
-          <CustomDescription
-            fieldName="publisher"
-            className="publisher"
-            showFieldName
-            value={publisher}
-          />
-          <CustomDescription
-            fieldName="date of publish"
-            className="dateOfPublish"
-            showFieldName
-            value={publishedDate}
-          />
-        </article>
-        <div className="btn-container">
-          <Link to="/my-bookshelf">
-            <button className="btn remove-btn" onClick={() => removeBook(id)}>
-              remove from favourites
-            </button>
-          </Link>
-          <button className="btn edit-btn">save changes</button>
-          <Link to="/my-bookshelf">
-            <button className="btn">back</button>
-          </Link>
-        </div>
+        <SingleBookDescriptions bookInfo={singleFavouriteBook} />
+        <SingleFavouriteBookButtons id={id} />
       </div>
+      <SingleFavouriteBookEdit />
     </Wrapper>
   );
 };
