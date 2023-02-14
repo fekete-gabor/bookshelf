406d3b445a30554532f04ff2e6f0856372be8364
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-14 15:27:39 +0100

    added add icon to every button

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,16 +1,22 @@
+import { IoMdAdd } from "../utils/icons";
 import { useAppContext } from "../context/app_context";
 
 const CustomButton = ({ name, className, handleChange }) => {
-  const { categoryName } = useAppContext();
+  const { categoryName, showForm } = useAppContext();
 
   return (
-    <button
-      className={categoryName === name ? `${className} active-btn` : className}
-      data-title={name}
-      onClick={handleChange}
-    >
-      {name}
-    </button>
+    <div className="category-btn-container">
+      <button
+        className={
+          categoryName === name ? `${className} active-btn` : className
+        }
+        data-title={name}
+        onClick={handleChange}
+      >
+        {name}
+      </button>
+      <IoMdAdd onClick={() => showForm()} className="add-icon" />
+    </div>
   );
 };
 
