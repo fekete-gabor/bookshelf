79c024913bef87239aca9b679a3105f0a5967a65
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-14 15:28:14 +0100

    added styling and add icon animation

--- src/components/EditCategoryButtons.js
+++ src/components/EditCategoryButtons.js
@@ -1,11 +1,15 @@
+import { useEffect } from "react";
 import { CustomButton } from "./index";
 import { useAppContext } from "../context/app_context";
 import styled from "styled-components";
+import useMediaQuery from "../utils/mediaQuery";
+import { gsap } from "gsap/dist/gsap";
 
 const BookCategoryButtons = ({ currentInput, setCurrentCategory }) => {
   const {
     isModal,
     openModal,
+    categoryName,
     changeCategory,
     isFormVisible,
     hideForm,
@@ -14,6 +18,7 @@ const BookCategoryButtons = ({ currentInput, setCurrentCategory }) => {
 
   const message =
     "You have an unsaved field, if you continue it's values will be lost. Do you want to proceed?";
+  const mediaQuery = useMediaQuery("(min-width: 1300px)");
 
   const { notification } = isModal;
 
@@ -37,44 +42,120 @@ const BookCategoryButtons = ({ currentInput, setCurrentCategory }) => {
     }
   };
 
+  useEffect(() => {
+    if (mediaQuery) {
+      gsap.utils.toArray(".category-btn-container").forEach((container) => {
+        const categoryBtn = container.children[0];
+        const category = categoryBtn.dataset.title;
+        const addBtn = container.children[1];
+        gsap.set(addBtn, { autoAlpha: 0, x: "-50px" });
+
+        if (category === categoryName) {
+          gsap.to(addBtn, {
+            duration: 0.25,
+            ease: "Expo.easeOut",
+            x: "0px",
+            autoAlpha: 1,
+          });
+        }
+      });
+    }
+  }, [categoryName, mediaQuery]);
+
   return (
     <Wrapper>
-      <CustomButton name="places" className="btn" handleChange={changeTitle} />
+      <CustomButton
+        name="places"
+        className="btn category-btn"
+        handleChange={changeTitle}
+      />
       <CustomButton
         name="characters"
-        className="btn"
+        className="btn category-btn"
         handleChange={changeTitle}
       />
       <CustomButton
         name="chapters"
-        className="btn"
+        className="btn category-btn"
         handleChange={changeTitle}
       />
-      <CustomButton name="items" className="btn" handleChange={changeTitle} />
+      <CustomButton
+        name="items"
+        className="btn category-btn"
+        handleChange={changeTitle}
+      />
       <CustomButton
         name="definitions"
-        className="btn"
+        className="btn category-btn"
         handleChange={changeTitle}
       />
-      <CustomButton name="dates" className="btn" handleChange={changeTitle} />
-      <CustomButton name="events" className="btn" handleChange={changeTitle} />
+      <CustomButton
+        name="dates"
+        className="btn category-btn"
+        handleChange={changeTitle}
+      />
+      <CustomButton
+        name="events"
+        className="btn category-btn"
+        handleChange={changeTitle}
+      />
       <CustomButton
         name="factions"
-        className="btn"
+        className="btn category-btn"
         handleChange={changeTitle}
       />
       <CustomButton
         name="my rating"
-        className="btn"
+        className="btn category-btn"
         handleChange={changeTitle}
       />
     </Wrapper>
   );
 };
 const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
   text-align: center;
+
   .active-btn {
     background: yellowgreen;
+    z-index: 2;
+  }
+
+  .category-btn-container {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+    transition: var(--transition);
+    .add-icon {
+      opacity: 0;
+      background: dodgerblue;
+      color: whitesmoke;
+      border-radius: 15px;
+      font-size: 2rem;
+      transition: var(--transition);
+      display: none;
+      z-index: 1;
+      &:hover {
+        background: salmon;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1300px) {
+    display: grid;
+
+    .category-btn-container {
+      gap: 1rem;
+      .add-icon {
+        display: block;
+        &:hover {
+          background: salmon;
+        }
+      }
+    }
   }
 `;
 export default BookCategoryButtons;
