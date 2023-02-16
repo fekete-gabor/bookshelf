cf3d3b8e5abc84789bf2b6b734157c112dab46df
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-16 14:05:12 +0100

    updated handleChange, added useEffect

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,13 +1,15 @@
 import { CustomInput, CustomTextArea } from "./index";
 import { useAppContext } from "../context/app_context";
+import styled from "styled-components";
+import { useEffect } from "react";
 
 const EditForm = ({ currentInput, setCurrentInput, setCurrentCategory }) => {
   const {
+    isEditing,
     stopEditing,
     isFormVisible,
     hideForm,
     updateInputList,
-    increaseCounter,
     categoryName,
   } = useAppContext();
 
@@ -22,13 +24,16 @@ const EditForm = ({ currentInput, setCurrentInput, setCurrentCategory }) => {
   };
 
   const handleChange = (e) => {
-    setCurrentInput({ ...currentInput, [e.target.name]: e.target.value });
+    if (e?.target?.name !== undefined) {
+      setCurrentInput({ ...currentInput, name: e.target.value });
+    } else {
+      setCurrentInput({ ...currentInput, desc: e });
+    }
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      await increaseCounter();
       await updateInputList({
         category: categoryName,
         inputs: [currentInput],
@@ -40,36 +45,93 @@ const EditForm = ({ currentInput, setCurrentInput, setCurrentCategory }) => {
     }
   };
 
+  useEffect(() => {
+    const { desc } = currentInput;
+    if (desc.startsWith("<p><br></p>") && !isEditing.status) {
+      // Quill automatically injects a blank paragraph on render & on page load,
+      // this useEffect replaces it with an empty value
+      setCurrentInput({
+        name: "",
+        desc: currentInput.desc
+          .replace(/(^([ ]*<p><br><\/p>)*)|((<p><br><\/p>)*[ ]*$)/gi, "")
+          .trim(" "),
+      });
+    }
+  }, [currentInput, setCurrentInput, isEditing.status]);
+
   if (isFormVisible) {
     return (
-      <form onSubmit={onSubmit}>
-        <CustomInput
-          type="text"
-          name="name"
-          handleChange={handleChange}
-          value={currentInput.name}
-        />
-        <CustomTextArea
-          type="text"
-          name="desc"
-          handleChange={handleChange}
-          value={currentInput.desc}
-        />
-        <button
-          className="btn"
-          type="submit"
-          disabled={
-            currentInput.name.length === 0 || currentInput.desc.length === 0
-          }
-        >
-          save
-        </button>
-        <button className="btn" type="button" onClick={() => resetInput()}>
-          remove
-        </button>
-      </form>
+      <Wrapper>
+        <form onSubmit={onSubmit}>
+          <CustomInput
+            type="text"
+            name="name"
+            label="name"
+            handleChange={handleChange}
+            value={currentInput.name}
+          />
+          <CustomTextArea
+            type="text"
+            name="desc"
+            label="description"
+            handleChange={handleChange}
+            value={currentInput.desc}
+          />
+          <button
+            className="btn"
+            type="submit"
+            disabled={
+              currentInput.name.length === 0 || currentInput.desc.length === 0
+            }
+          >
+            save
+          </button>
+          <button className="btn" type="button" onClick={() => resetInput()}>
+            remove
+          </button>
+        </form>
+      </Wrapper>
     );
   }
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 3rem;
+
+  form {
+    margin: 0 auto;
+    width: 55%;
+  }
+
+  .form-input {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    label,
+    input {
+      font-size: 1.5rem;
+      letter-spacing: 3px;
+      padding: 0.5rem;
+    }
+    input {
+      width: 50%;
+      border: solid 1px plum;
+      border-radius: 15px;
+      transition: var(--transition);
+      outline: none;
+      &:focus {
+        border: solid 1px var(--primary-clr-2);
+      }
+    }
+    .textarea {
+      width: 75%;
+      max-width: 700px;
+      background-color: white;
+      margin: 1rem 0;
+    }
+  }
+`;
 
 export default EditForm;
