f7fcf19c8a5ce9e478c52905d7517689350eb2f6
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-28 15:58:50 +0100

    fixed small bug

--- src/utils/removeHTMLTags.js
+++ src/utils/removeHTMLTags.js
@@ -1,6 +1,6 @@
 export const removeHTMLTags = (str) => {
-  if (str === null || str === "") return false;
-  str = str.toString();
+  if (str === null || str === "" || str === undefined) return false;
+  str = str?.toString();
 
   return str.replace(/(<([^>]+)>)/gi, "");
 };
