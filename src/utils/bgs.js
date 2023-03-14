a11e5f295508d5abded50609647e9a5b9ac42daa
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-14 17:15:24 +0100

    created array

---
+++ src/utils/bgs.js
@@ -0,0 +1,31 @@
+import bg1 from "../assets/bg.jpg";
+import bg2 from "../assets/bg2.jpg";
+import bg3 from "../assets/bg3.jpg";
+import bg4 from "../assets/bg4.jpg";
+import bg5 from "../assets/bg5.jpg";
+import bg6 from "../assets/bg6.jpg";
+import bg7 from "../assets/bg7.jpg";
+import bg8 from "../assets/bg8.jpg";
+import bg9 from "../assets/bg9.jpg";
+import bg10 from "../assets/bg10.jpg";
+import bg11 from "../assets/bg11.jpg";
+import bg12 from "../assets/bg12.jpg";
+import bg13 from "../assets/bg13.jpg";
+
+const tempArray = [
+  bg1,
+  bg2,
+  bg3,
+  bg4,
+  bg5,
+  bg6,
+  bg7,
+  bg8,
+  bg9,
+  bg10,
+  bg11,
+  bg12,
+  bg13,
+];
+
+export default tempArray;
