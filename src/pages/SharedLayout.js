cafe1ce2748e553f06d6ed1d77d69004c4a6fbed
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-14 17:12:43 +0100

    added backgrounds component

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,10 +1,18 @@
-import { Navbar, Sidebar, Modal, Footer, ScrollTop } from "../components";
+import {
+  Navbar,
+  Sidebar,
+  Modal,
+  Footer,
+  ScrollTop,
+  Backgrounds,
+} from "../components";
 import { Outlet } from "react-router-dom";
 
 const SharedLayout = () => {
   return (
     <>
       <Navbar />
+      <Backgrounds />
       <Sidebar />
       <Modal />
       <Outlet />
