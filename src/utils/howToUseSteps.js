b69eaf5fc7d9f6eff200f22b1532d1f5cf56bb43
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-14 17:15:32 +0100

    created array

---
+++ src/utils/howToUseSteps.js
@@ -0,0 +1,35 @@
+import img1 from "../assets/how_to_use_0.png";
+import img2 from "../assets/how_to_use_1.png";
+import img3 from "../assets/how_to_use_2.png";
+import img4 from "../assets/how_to_use_3.png";
+import img5 from "../assets/how_to_use_4.png";
+
+const array = [
+  {
+    id: 1,
+    img: img1,
+    text: `Go to the "Search" tab.`,
+  },
+  {
+    id: 2,
+    img: img2,
+    text: `Search for your favourite books.`,
+  },
+  {
+    id: 3,
+    img: img3,
+    text: `Clicking either of these buttons will add the book to your shelf.`,
+  },
+  {
+    id: 4,
+    img: img4,
+    text: `Go to the "My Bookshelf", click on a book, then "My notes" tab.`,
+  },
+  {
+    id: 5,
+    img: img5,
+    text: `Create a category, click on the plus sign, then add as many notes as you want. :)`,
+  },
+];
+
+export default array;
