7d53e032ff90f399498720a4fcb2a9cc76a9f5d0
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-28 14:10:39 +0200

    changed not found img

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import notFound from "../assets/404.png";
+import notFound from "../assets/no_img_found.png";
 import { Link, Navigate } from "react-router-dom";
 import {
   AiFillHeart,
