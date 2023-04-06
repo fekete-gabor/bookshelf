b245946af2855e32a5ad3ff12bc95e6573aff221
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-04-06 11:52:42 +0200

    changed logic

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -82,7 +82,7 @@ const SearchForm = ({ fetchFromGoogle }) => {
               (fetchFromGoogle &&
                 searchAuthor.length === 0 &&
                 searchTerm.length === 0) ||
-              (!fetchFromGoogle && searchAuthor.length >= 2)
+              (!fetchFromGoogle && searchAuthor.length <= 2)
                 ? "disabled submit-btn"
                 : "submit-btn"
             }
@@ -92,7 +92,7 @@ const SearchForm = ({ fetchFromGoogle }) => {
               (fetchFromGoogle &&
                 searchAuthor.length === 0 &&
                 searchTerm.length === 0) ||
-              (!fetchFromGoogle && searchAuthor.length >= 2)
+              (!fetchFromGoogle && searchAuthor.length <= 2)
             }
           >
             <SlMagnifier />
